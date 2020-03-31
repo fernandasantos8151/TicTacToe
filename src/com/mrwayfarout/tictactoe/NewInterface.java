fe02727747d970951ba069fc7f40f8d931137e78
Fernanda <Fernanda@Fernanda>
2020-03-30 21:52:42 -0300

    Commit incial do projeto

---
+++ src/com/mrwayfarout/tictactoe/NewInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mrwayfarout.tictactoe;
+
+/**
+ *
+ * @author RUTH
+ */
+public interface NewInterface {
+
+    void run();
+    
+    
+}
